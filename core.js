e106a6d73035f3d386f32ab065f014950c646a21
Giang Vu <giangvdm15@gmail.com>
2017-09-03 09:29:58 +0700

    Complete Accordion

--- core.js
+++ core.js
@@ -10,3 +10,11 @@ function checkEmpty(field) {
 function disableButtonsUponReset() {
     document.getElementById("postBtn").disabled = true;
 }
+
+function openAccordion(obj) {
+    // obj.nextElementSibling.style.display
+    if (obj.nextElementSibling.style.display !== "block") {
+        obj.nextElementSibling.style.display = "block";
+    }
+    else (obj.nextElementSibling.style.display = "none");
+}
